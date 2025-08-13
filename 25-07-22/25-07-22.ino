7ae9eae89ba00a7f2e228b5ae17181c19fa534fe
Pedro Andrade <p.abrantesandrade@gmail.com>
2025-08-13 16:28:35 -0300

    first commit

---
+++ 25-07-22/25-07-22.ino
@@ -0,0 +1,66 @@
+#include <Servo.h>
+
+// Criando objetos Servo para cada dedo
+Servo servoMindinho; //amarelo
+Servo servoAnelar; // azul
+Servo servoMedio; // verde escuro
+Servo servoIndicador; // roxo
+Servo servoPolegar; // laranja
+Servo servoPulso; // branco
+
+// void a(){
+//   servoMindinho.write(180);
+//   servoAnelar.write(180);
+//   servoMedio.write(180);
+//   servoIndicador.write(180);
+//   servoPolegar.write(180);
+//   servoPulso.write(180);
+// }
+
+void setup() {
+// Anexando os servos aos pinos definidos
+  servoMindinho.attach(5);
+  servoAnelar.attach(6);
+  servoMedio.attach(7);
+  servoIndicador.attach(8);
+  servoPolegar.attach(9);
+  servoPulso.attach(10);
+  delay(1000);
+}
+
+void loop() {
+  // servoMindinho.write(180);
+  // delay(1000);
+  // servoMindinho.write(0);
+  // delay(1000);
+    
+  // // Anelar
+  // servoAnelar.write(180);
+  // delay(1000);
+  // servoAnelar.write(0);
+  // delay(1000);
+
+  // // Médio
+  // servoMedio.write(180);
+  // delay(1000);
+  // servoMedio.write(0);
+  // delay(1000);
+
+  // // Indicador
+  // servoIndicador.write(180);
+  // delay(1000);
+  // servoIndicador.write(0);
+  // delay(1000);
+
+  // // Polegar
+  // servoPolegar.write(180);
+  // delay(1000);
+  // servoPolegar.write(0);
+  // delay(1000);
+
+  // Pulso
+  servoPulso.write(180); //90 = 45 graus| 180 = 90 graus (sentido horário)
+  delay(1000);
+  servoPulso.write(0);
+  delay(1000);
+}
